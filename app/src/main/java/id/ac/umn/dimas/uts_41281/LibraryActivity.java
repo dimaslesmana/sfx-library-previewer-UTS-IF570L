78e45b82f1f96e4fe2a0ff479a92bee321cadeec
Dimas Lesmana <dimaslesmana99@gmail.com>
2021-10-08 00:54:48 +0700

    Initial Commit

---
+++ app/src/main/java/id/ac/umn/dimas/uts_41281/LibraryActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.dimas.uts_41281;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+    }
+}
---
+++ app/src/main/java/id/ac/umn/dimas/uts_41281/LoginActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.dimas.uts_41281;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
